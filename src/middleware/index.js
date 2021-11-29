b2a6489b85d986f9c9effce4e33efbbc599d9b7d
adeisbright <adenababanla@gmail.com>
2021-11-29 04:12:40 +0100

    Add linting rule and error handling middleware
    Use eslint for static code analysis and to ensure the code is free
    from bad coding styles.
    
    Create the middleware for handing error. The middleware should be
    responsible for differentiating betweeen programmer , user , and/or
    operational errors

---
+++ src/middleware/index.js
@@ -0,0 +1,18 @@
+const {
+    ApplicationError,
+    BadRequestError,
+    NotAuthorizeError,
+    NotFoundError,
+    DbError,
+    ForbiddenError,
+    errorParser,
+} = require("./ErrorHandler");
+module.exports = {
+    ApplicationError,
+    BadRequestError,
+    NotAuthorizeError,
+    NotFoundError,
+    DbError,
+    ForbiddenError,
+    errorParser,
+};
